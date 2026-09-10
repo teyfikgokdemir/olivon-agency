ac231d56f90d3a78c2f7083fcbd9401445cfd537
teyfikgokdemir <teyfikgokdemir@outlook.com>
2026-09-10 06:20:23 +0300

    Harden robots directives for search crawlers

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,17 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: ["Googlebot", "Bingbot"],
+        allow: "/",
+      },
+    ],
     sitemap: "https://olivon.com.tr/sitemap.xml",
-    host: "https://olivon.com.tr",
+    host: "olivon.com.tr",
   };
 }
